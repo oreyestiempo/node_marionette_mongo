84ae0c8437791ad757000543106acb1e718a85c7
ahernandez-tiempodev <ahernandez@tiempodevelopment.com>
2014-06-10 12:35:45 -0700

    Ejemplo Alejandro

---
+++ public/javascript/modules/task1-alex/views/table-list.js
@@ -0,0 +1,14 @@
+define([
+    'lib/backbone',
+    'lib/underscore',
+    'modules/task1-alex/views/main',
+    'text!/javascript/modules/task1-alex/templates/main.html'],
+    function(Backbone, _, ItemListView, template) {
+        var ListView = Backbone.Marionette.CompositeView.extend({
+            template: _.template(template),
+            itemView: ItemListView,
+            itemViewContainer: '#table-list tbody'
+        });
+
+    return ListView;
+    });
